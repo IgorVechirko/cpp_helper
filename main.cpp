12e99b32434f20e01ece84ef527c2a111bb1a556
IgorVechirko <vechirkoigor@gmail.com>
2020-07-30 09:29:29 +0300

    add not finish RTTIemulation example

--- main.cpp
+++ main.cpp
@@ -2,10 +2,9 @@
 #include <vector>
 #include <string>
 #include <ctime>
+#include <iterator>
 
 #define LOGOUT_FUNC std::cout << __FUNCTION__ << std::endl;
-
-
 
 
 namespace fucnctionsAndMethodsPointers
@@ -47,6 +46,126 @@ namespace fucnctionsAndMethodsPointers
 		(instance.*methodPtr)( 5, &arg2 );
 	}
 };
+
+/*void* operator new ( std::size_t count )
+{
+	std::cout << "operator new" << std::endl;
+	return malloc(count);
+}
+void* operator new ( std::size_t count, const std::nothrow_t& tag )
+{
+	std::cout << "operator new ( nothrow )" << std::endl;
+	return malloc(count);
+}
+
+void* operator new[] ( std::size_t count )
+{
+	std::cout << "operator new[]" << std::endl;
+	return malloc(count);
+}
+void* operator new[] ( std::size_t count, const std::nothrow_t& tag )
+{
+	std::cout << "operator new[] ( nothrow )" << std::endl;
+	return malloc(count);
+}
+
+void* operator new ( std::size_t count, int num )
+{
+	std::cout << "placement operator new num = " << num << std::endl;
+
+	return new char[count];
+}*/
+
+
+namespace memoryAllocations
+{
+	class TestAllocation
+	{
+		int _member;
+
+		public:
+
+			TestAllocation()
+				: _member( 0 )
+			{LOGOUT_FUNC};
+			TestAllocation( int member )
+				: _member( member )
+			{};
+			virtual ~TestAllocation(){LOGOUT_FUNC};
+	};
+
+	class OverrideOperatorNewAllocation
+	{
+		public:
+
+			OverrideOperatorNewAllocation(){LOGOUT_FUNC};
+			virtual ~OverrideOperatorNewAllocation(){LOGOUT_FUNC};
+
+			
+			void* operator new( size_t size )
+			{
+				LOGOUT_FUNC
+				void* result = new char[sizeof(OverrideOperatorNewAllocation)];
+
+				return result;
+			}
+	};
+
+	void exampleNewExpression()
+	{
+		LOGOUT_FUNC
+
+		auto intsArray = new( int[10] );
+		auto intsArray2 = new int[10];
+		auto funcPointersArray = new ( int(*[10])(int, float) );
+		//auto funcPintersArray = new int(*[10])(int, float); error (new int) (*[10]())
+
+		delete[] intsArray;
+		delete[] intsArray2;
+		delete[] funcPointersArray;
+
+		auto allocateWithInitializer = new TestAllocation();
+		auto allocateWithoutInitializer = new TestAllocation;
+		auto allocateWithAutoType = new auto( *allocateWithoutInitializer );
+
+		delete allocateWithInitializer;
+		delete allocateWithoutInitializer;
+		delete allocateWithAutoType;
+	}
+
+	void examplePlacementNew()
+	{
+		LOGOUT_FUNC
+
+		auto memoryForObject = new char[sizeof(TestAllocation)];
+		auto createByPlacementNew = new (memoryForObject)(TestAllocation);
+
+		std::cout << "memoryForObject address = " << (void*)memoryForObject << " createByPlacementNew obj address = " << createByPlacementNew << std::endl; 
+
+		createByPlacementNew->~TestAllocation();
+		delete [] memoryForObject;
+	}
+
+	void exampleNewOperatorOverrides()
+	{
+		auto newExpressionOverride = new TestAllocation();
+		auto newNothrowExpressionOverride = new(std::nothrow) TestAllocation();
+		auto newArrayExpressionOverride = new TestAllocation[2];
+		auto newNothrowArrayExpressionOverride = new(std::nothrow) TestAllocation[2];
+
+		delete newExpressionOverride;
+		delete newNothrowExpressionOverride;
+		delete[] newArrayExpressionOverride;
+		delete[] newNothrowArrayExpressionOverride;
+
+		auto memoryForObject = new char[sizeof(TestAllocation)];
+		auto placementCreatedObj = new ( memoryForObject ) (TestAllocation);
+
+		placementCreatedObj->~TestAllocation();
+		delete[] memoryForObject;
+	}
+
+}
 
 namespace smartPointers
 {
@@ -261,13 +380,16 @@ namespace templates
 	template< typename T, typename... TypesPackage >
 	void tempalteFunctionWithTypesPackage(const T& firstArg, const TypesPackage&... argsPackage )
 	{
+		std::cout << "args count = " << sizeof...(TypesPackage) << std::endl;
+
 		std::cout << firstArg << std::endl;
 		tempalteFunctionWithTypesPackage( argsPackage... );
 	}
 
 	void example()
 	{
-		tempalteFunctionWithTypesPackage( "sldkfj", 3, 6.598f, true );
+		tempalteFunctionWithTypesPackage( "", 5, 0.054f, true );
+		tempalteFunctionWithTypesPackage( 5, 0.054, true );
 	}
 	
 
@@ -329,6 +451,7 @@ namespace debugTools
 		LOGOUT_FUNC
 	}
 };
+
 
 namespace ClassesAndStructures
 {
@@ -553,9 +676,273 @@ namespace FinalKeyWord
 }
 
 
+namespace RTTIEmulation
+{
+	/*void collectTypes( std::vector<int>& collection )
+	{
+		LOGOUT_FUNC
+	}
+
+	template< typename First >
+	void collectTypes( std::vector<int>& collection )
+	{
+		auto resultTypes = First::getTypesList();
+		if ( resultTypes )
+		{
+			std::copy( resultTypes->begin(), resultTypes->end(), std::inserter( collection, collection.end() ) );
+		}
+	}
+
+	template< typename First, typename... Others >
+	void collectTypes( std::vector<int>& collection, First* firstArg )
+	{
+		auto resultTypes = First::getTypesList();
+		if ( resultTypes )
+		{
+			std::copy( resultTypes->begin(), resultTypes->end(), std::inserter( collection, collection.end() ) );
+		}
+		openTypesPack<Others...>(collection);
+	}
+
+	template< typename First, typename... Others >
+	void openTypesPack( std::vector<int>& collection )
+	{
+		collectTypes( collection, (First*)nullptr );
+	}
+
+	#define GET_TYPES_LIST_FUNC_1(__TYPE_ID__)\
+	static const std::vector<int>* getTypesList()\
+	{\
+		static std::vector<int> typesList;\
+		if( typesList.empty() )\
+		{\
+			typesList.push_back( __TYPE_ID__ );\
+		}\
+		return &typesList;\
+	}\
+
+	#define GET_TYPES_LIST_FUNC_2(__TYPE_ID__, ...)\
+	static const std::vector<int>* getTypesList()\
+	{\
+		static std::vector<int> typesList;\
+		if( typesList.empty() )\
+		{\
+			openTypesPack<__VA_ARGS__>(typesList);\
+			typesList.push_back( __TYPE_ID__ );\
+		}\
+		return &typesList;\
+	}\*/
+	
+	void copyVecs( std::vector<int>& output, const std::vector<int>& input )
+	{
+		std::copy( input.rbegin(), input.rend(), std::inserter( output, output.end() ) );
+	}
+	
+
+	class FirstTreeBase
+	{
+
+		public:
+
+			static int getTypeID(){return 0;};
+			static const std::vector<int>& getTypesList()
+			{
+				static std::vector<int> typesList;
+				if( typesList.empty() )
+				{
+					typesList.push_back( getTypeID() );
+				}
+
+				return typesList;
+			}
+
+			FirstTreeBase(){};
+			virtual ~FirstTreeBase(){};
+	};
+
+	class FirstTreeChild : public FirstTreeBase
+	{
+		public:
+
+			static int getTypeID(){return 1;};
+			static const std::vector<int>& getTypesList()
+			{
+				static std::vector<int> typesList;
+				if( typesList.empty() )
+				{
+					typesList.push_back( getTypeID() );
+					copyVecs( typesList, FirstTreeBase::getTypesList() );
+				}
+
+				return typesList;
+			}
+
+			FirstTreeChild(){};
+			virtual ~FirstTreeChild(){};
+	};
+
+	class FirstTreeSecondaryChild : public FirstTreeChild
+	{
+		public:
+
+				static int getTypeID(){return 2;};
+				static const std::vector<int>& getTypesList()
+				{
+					static std::vector<int> typesList;
+					if( typesList.empty() )
+					{
+						typesList.push_back( getTypeID() );
+						copyVecs( typesList, FirstTreeChild::getTypesList() );
+					}
+
+					return typesList;
+				}
+
+				FirstTreeSecondaryChild(){};
+				virtual ~FirstTreeSecondaryChild(){};
+	};
+
+
+
+	class SecondTreeBase
+	{
+		public:
+
+			static int getTypeID(){return 3;};
+			static const std::vector<int>& getTypesList()
+				{
+					static std::vector<int> typesList;
+					if( typesList.empty() )
+					{
+						typesList.push_back( getTypeID() );
+					}
+
+					return typesList;
+				}
+
+			SecondTreeBase(){};
+			virtual ~SecondTreeBase(){};
+	};
+
+	class SecondTreeChild : public SecondTreeBase
+	{
+		public:
+
+			static int getTypeID(){return 4;};
+			static const std::vector<int>& getTypesList()
+			{
+				static std::vector<int> typesList;
+				if( typesList.empty() )
+				{
+					typesList.push_back( getTypeID() );
+					copyVecs( typesList, SecondTreeBase::getTypesList() );
+				}
+
+				return typesList;
+			}
+
+			SecondTreeChild(){};
+			virtual ~SecondTreeChild(){};
+	};
+
+	class SecondTreeSecondaryChild : public SecondTreeChild
+	{
+		public:
+
+				static int getTypeID(){return 5;};
+				static const std::vector<int>& getTypesList()
+				{
+					static std::vector<int> typesList;
+					if( typesList.empty() )
+					{
+						typesList.push_back( getTypeID() );
+						copyVecs( typesList, SecondTreeChild::getTypesList() );
+					}
+
+					return typesList;
+				}
+
+				SecondTreeSecondaryChild(){};
+				virtual ~SecondTreeSecondaryChild(){};
+	};
+
+
+	class TreesCombine 
+		: public SecondTreeSecondaryChild
+		, public FirstTreeSecondaryChild
+	{
+		public:
+			
+			static int getTypeID(){return 6;};
+			static const std::vector<int>& getTypesList()
+			{
+				static std::vector<int> typesList;
+				if( typesList.empty() )
+				{
+					typesList.push_back( getTypeID() );
+					copyVecs( typesList, SecondTreeSecondaryChild::getTypesList() );
+					copyVecs( typesList, FirstTreeSecondaryChild::getTypesList() );
+				}
+
+				return typesList;
+			}
+
+			TreesCombine(  ){};
+			virtual ~TreesCombine(){};
+	};
+
+	template<typename T, typename... Types>
+	T* createCastedObj( const Types&... args )
+	{
+		auto ptrSize = sizeof(const std::vector<int>*);
+		auto charsArray = new char[sizeof(T) + ptrSize];
+
+		auto headerMemory = reinterpret_cast<const std::vector<int>*>(charsArray);
+		auto objMemory = reinterpret_cast<T*>(charsArray+ptrSize);
+
+		headerMemory = &T::getTypesList();
+
+		auto result = new(objMemory) T(args...);
+
+		return result;
+	}
+
+	template< typename T, typename Y>
+	T* my_dynamic_cast(Y* convertPtr )
+	{
+		if( !convertPtr )
+			return nullptr;
+		
+		auto objClassTypesList = reinterpret_cast<const std::vector<int>*>( reinterpret_cast<char*>(convertPtr) - sizeof(const std::vector<int>*) );
+		
+		auto findIt = std::find( objClassTypesList->begin(), objClassTypesList->end(), T::getTypeID() );
+		if ( findIt != objClassTypesList->end() )
+		{
+			return reinterpret_cast<T*>(convertPtr);
+		}
+
+		return nullptr;
+	};
+
+	void example()
+	{
+		LOGOUT_FUNC
+
+		auto firstTreeChildInstance = static_cast<FirstTreeChild*>( createCastedObj<TreesCombine>() );
+
+		auto secondTreeChildInstance = my_dynamic_cast<SecondTreeChild>(firstTreeChildInstance);
+
+		if ( secondTreeChildInstance )
+			std::cout << "cast successful" << std::endl;
+	}
+
+};
+
+
 int main()
 {
-	FinalKeyWord::example();
+
+	RTTIEmulation::example();
 
 	std::cin.get();
 
