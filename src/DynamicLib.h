2eb7b684445199deece2b718e101a5383ae02ccc
IgorVechirko <vechirkoigor@gmail.com>
2020-08-21 16:19:24 +0300

    add using dynamic library

--- src/DynamicLib.h
+++ src/DynamicLib.h
@@ -1,10 +1,16 @@
 #ifndef DynamicLib_H
 #define DynamicLib_H
 
+#ifdef _WINDOWS
+
 #ifdef DYNAMIC_LIB_EXPORT
 	#define DYNAMIC_LIB_API __declspec(dllexport)
 #else
 	#define DYNAMIC_LIB_API __declspec(dllimport)
+#endif
+
+#else
+	define DYNAMIC_LIB_API
 #endif
 
 
