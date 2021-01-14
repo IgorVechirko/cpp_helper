e8677abe6a8c53b560ba1d1c355cfc7275d2fe64
IgorVechirko <vechirkoigor@gmail.com>
2021-01-14 21:51:35 +0200

    update examples

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,10 @@
 #include <iostream>
 
-/*#include "FucnctionsAndMethodsPointers.h"
-//#include "MemoryAllocations.h"//here override new/delete operators
-#include "SmartPointers.h"
-#include "Templates.h"
-#include "Lambdas.h"
-#include "DebugTools.h"
-#include "ClassesAndStructures.h"
-#include "VirtualAndOverrideKeyWords.h"
-#include "FinalKeyWord.h"
-#include "RTTIEmulation.h"
-#include "RvalueLvalueSemantic.h"
-#include "InlineFunctions.h"
-#include "StaticLibUsing.h"
-#include "DynamicLibUsing.h"
-#include "ExplicitKeyWord.h"
-#include "MutableKeyWord.h"
-#include "ValotileRegisterKeyWords.h"
-#include "ExternKeyWord.h"
-#include "NumericLimits.h"
-#include "MinMaxFuncs.h"
-#include "Ratio.h"
-#include "Chrono.h"
-#include "Random.h"
-#include "ParallelProgramming.h"
-#include "Containers.h"
-#include "Iterators.h"
+
+//stl
 #include "Algorithms.h"
-#include "OperatorsOverrides.h"
-#include "Attributes.h"
-#include "Namespaces.h"
-#include "Exceptions.h"
-#include "FreeListAllocator.h"*/
+#include "Iterators.h"
+#include "Containers.h"
 
 #include "Attributes.h"
 #include "ClassesAndStructures.h"
@@ -45,12 +18,22 @@
 #include "Chrono.h"
 #include "Random.h"
 #include "OperatorsOverrides.h"
+#include "Exceptions.h"
+#include "ExplicitKeyWord.h"
+#include "ExternKeyWord.h"
+#include "FinalKeyWord.h"
+#include "MinMaxFuncs.h"
+#include "MutableKeyWord.h"
+#include "Namespaces.h"
+#include "NumericLimits.h"
+#include "ValotileRegisterKeyWords.h"
+#include "VirtualAndOverrideKeyWords.h"
 
 
 
 int main()
 {
-	OperatorsOverrides::example();
+	VirtualAndOverrideKeyWords::example();
 
 	std::cin.get();
 
